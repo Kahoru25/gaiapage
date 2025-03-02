94b6fc896b4e77520db575812a5e325961563e2a
Jeysson Gonzalez <jeyfergo25@gmail.com>
2025-03-02 14:33:39 -0500

    fix_new7

--- next.config.mjs
+++ next.config.mjs
@@ -3,40 +3,44 @@
 const nextConfig = {
   output: "standalone",
   webpack(config) {
-    // Agregar una regla para manejar archivos de imagen
-    config.module.rules.push(
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'static/images/',
-              publicPath: '/_next/static/images/',
-            },
-          },
-        ],
-      },
-      // Mantener las reglas existentes para SVG
-      {
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-        use: ['file-loader'],
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [/url/] },
-        use: ['@svgr/webpack'],
-      }
-    );
-
-    // Excluir archivos .svg de la regla existente
+    // Mover la inicialización de fileLoaderRule al principio
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.(".svg")
     );
+
+    // Asegúrate de que fileLoaderRule exista antes de usarlo
     if (fileLoaderRule) {
+      config.module.rules.push(
+        // Reaplicar la regla existente, pero solo para importaciones de SVG que terminen en ?url
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/, // *.svg?url
+        },
+        // Convertir todas las demás importaciones de *.svg a componentes de React
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: { not: [...(fileLoaderRule.resourceQuery.not || []), /url/] }, // excluir si *.svg?url
+          use: ["@svgr/webpack"],
+        },
+        // Agregar una regla para manejar archivos PNG
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]",
+                outputPath: "static/images/",
+                publicPath: "/_next/static/images/",
+              },
+            },
+          ],
+        }
+      );
+
+      // Modificar la regla de carga de archivos para ignorar *.svg
       fileLoaderRule.exclude = /\.svg$/i;
     }
 
